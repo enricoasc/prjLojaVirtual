e9b16b948e6d211daa6d5c8d26dddf710724bcc8
Enrico Augusto <enricoasc@gmail.com>
2024-01-19 02:04:40 -0300

    Primeiro Projeto de Python Orientado a Objetos : Cliente

--- Main.py
+++ Main.py
@@ -2,8 +2,13 @@ class Main:
     pass
 
 from Cliente import Cliente
+from Conta import Conta
 
 c1 = Cliente('Enrico','34 98892-6898')
 
 print(c1)
 print(c1.nome, ' e ', c1.telefone)
+
+conta = Conta(c1.nome,2526, 0)
+
+print(conta.titular," Numero: ",conta.numero," Saldo: ",conta.saldo)
