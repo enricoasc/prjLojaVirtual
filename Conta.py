a33cd761dbde91ea0093b86f88a6f7503e1cbd08
Enrico Augusto <enricoasc@gmail.com>
2024-01-20 10:56:17 -0300

    Primeiro Projeto de Python Orientado a Objetos : Encapsulamento , Getter , setter

--- Conta.py
+++ Conta.py
@@ -1,5 +1,28 @@
 class Conta:
     def __init__(self, titular, numero, saldo ):
-        self.saldo = 0
+        self._saldo = 0
         self.numero = numero
         self.titular = titular
+
+    def get_saldo(self):
+        return self._saldo
+
+    def set_saldo(self,saldo):
+        if (saldo < 0):
+            print('O saldo não pode ser negativo')
+        else:
+            self._saldo = saldo
+
+    def saque(self,valor):
+        if(self._saldo>=valor):
+            self._saldo-=valor
+            print('Saque realizado com sucesso :..')
+        else:
+            print('Saque inuficiente :..')
+
+    def deposita(self,valor):
+        self._saldo+=valor
+
+
+    def extrato(self):
+        print("Cliente: ",self.titular, " Saldo atual: ", self.get_saldo() )
--- Main.py
+++ Main.py
@@ -7,8 +7,15 @@ from Conta import Conta
 c1 = Cliente('Enrico','34 98892-6898')
 
 print(c1)
-print(c1.nome, ' e ', c1.telefone)
+print(c1._nome, ' e ', c1._telefone)
 
-conta = Conta(c1.nome,2526, 0)
+conta = Conta(c1._nome,2526, 0)
 
-print(conta.titular," Numero: ",conta.numero," Saldo: ",conta.saldo)
+print(conta.titular," Numero: ",conta.numero," Saldo: ",conta._saldo)
+
+nome = "Enrico"
+print(len(nome))
+
+conta.deposita(100)
+conta.saque(99)
+conta.extrato()
